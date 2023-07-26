dc4f68528f43dfc827aec540b0555d6410dd4474
msandoval55 <116230991+msandoval55@users.noreply.github.com>
2023-07-26 10:35:16 -0500

    Create ADGroupMember-Members List.ps1

---
+++ Microsoft/Active Directory/ADUC/ADGroupMember-Members List.ps1
@@ -0,0 +1,115 @@
+function Green
+{
+process { Write-Host $_-ForegroundColor Green } 
+}
+function Red
+{
+process { Write-Host $_-ForegroundColor Red }
+}
+Write-Output
+"*********************************************************************************" | red
+Write-Output "↓ ↓ ↓ Title ↓ ↓ ↓"| Green Write-Output
+"*********************************************************************************" | red
+#
+#
+#
+#
+Get-ADGroupMember -Identity "ad.group.name" | Get-ADObject - Properties 
+Name,Title,Department,Description | Sort Department,Title | Format-Table Name,Title,Department,Description
+#
+#
+#
+#
+function Green
+{
+process { Write-Host $_-ForegroundColor Green } 
+}
+function Red
+{
+process { Write-Host $_-ForegroundColor Red }
+}
+Write-Output
+"*********************************************************************************" | red
+Write-Output "↓ ↓ ↓ Title ↓ ↓ ↓" | Green Write-Output
+"*********************************************************************************" | red
+#
+#
+#
+#
+Get-ADGroupMember -Identity "ad.group.name" | Get-ADOhject -Properties
+Name,Title,Department,Description | Sort Department,Title | Format-Table Name,Title,Department,Description
+#
+#
+#
+#
+function Green
+{
+process { Write-Host $_-ForgroundColor Green }
+}
+function Red
+{
+process { Write-Host $_-ForgroundColor Red }
+}
+Write-Output
+"*********************************************************************************" | red
+Write-Output "↓ ↓ ↓ Title ↓ ↓ ↓" | Green Write-Output
+"*********************************************************************************"  | red
+#
+#
+#
+#
+Get-ADGroupMember -Identity "ad.group.name" | Get-ADObject -Properties
+Name,Title,Department, Description | Sort Department,Title | Format-Table Name,Title,Department,Description
+#
+#
+#
+#
+function Green
+{
+process { Write-Host $_-ForegroundColor Green } 
+}
+function Red
+{
+process { Write-Host $_-ForegroundColor Red }
+}
+Write-Output
+"*********************************************************************************" | red
+Write-Output "↓ ↓ ↓ Title ↓ ↓ ↓" | Green Write-Output
+"*********************************************************************************" | red
+Get-ADGroupMember -Identity "ad.group.name" | Get-ADObject -Properties
+Name,Title,Department,Description | Sort Department,Title | Format-Table Name,Title,Department,Description
+#
+#
+#
+#
+function Green
+{
+process { Write-Host $_-ForegroundColor Green } 
+}
+function Red
+{
+process { Write-Host $_-ForegroundColor Red }
+}
+Write-Output
+"*********************************************************************************" | red
+Write-Output "↓ ↓ ↓ Title ↓ ↓ ↓" | Green Write-Output
+"*********************************************************************************" | red
+#
+#
+#
+#
+Get-ADGroupMember -Identity "ad.group.name" | Get-ADObject -Properties
+Name,Title,Department,Description | Sort Department,Title | Format-Table Name,Title,Department,Description
+#
+#
+#
+#
+function Green
+{
+process { Write-Host $_-ForegroundColor Green } 
+}
+function Red
+{
+process { Write-Host $_-ForegroundColor Red }
+}
+Write-Output
