f4201740da42f38dbf57a2c44da15f9675116822
msandoval55 <116230991+msandoval55@users.noreply.github.com>
2023-08-04 16:32:41 -0500

    Commit and Push

---
+++ Microsoft Exchange/Exchange-online/EXO V3 PS module Cmdlets.ps1
@@ -0,0 +1,11 @@
+#https://learn.microsoft.com/en-us/powershell/exchange/cmdlet-property-sets?view=exchange-ps
+#https://techcommunity.microsoft.com/t5/exchange-team-blog/deprecation-of-remote-powershell-rps-protocol-in-security-and/ba-p/3815432
+
+Version 2.0.5 and earlier is known as the Exchange Online PowerShell V2 module (abbreviated as the EXO V2 module). Version 3.0.0 and later is known as the Exchange Online PowerShell V3 module (abbreviated as the EXO V3 module).
+
+Connect-exchangeonline
+
+Get-EXOCasMailbox property sets
+Get-EXOMailbox property sets
+Get-EXOMailboxStatistics property sets
+Get-EXORecipient property sets
