4db1023e5f029c0502239b8e553d481f61707ebb
msandoval55 <116230991+msandoval55@users.noreply.github.com>
2023-08-15 14:35:59 -0500

    Update Get-InstalledApps.ps1

--- Microsoft Windows Server/Get-InstalledApps.ps1
+++ Microsoft Windows Server/Get-InstalledApps.ps1
@@ -1,2 +1,2 @@
-#Retrieve Installed Applications
+#Retrieve installed applications from remote server
 Invoke-Command -ComputerName "servername" -ScriptBlock {Get-WmiObject -Class Win32_Product | Select-Object Name,version,IdentifyingNumber | sort name | ft }
