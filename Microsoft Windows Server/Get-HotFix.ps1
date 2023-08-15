f64bbfdab3575f89b0307c7e0f0341b3b6ab5f0c
msandoval55 <116230991+msandoval55@users.noreply.github.com>
2023-08-15 15:50:01 -0500

    commit and push

--- Microsoft Windows Server/Get-HotFix.ps1
+++ Microsoft Windows Server/Get-HotFix.ps1
@@ -1,18 +1,7 @@
 #https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.management/get-hotfix?view=powershell-7.3
 #Show last patch/update time and date single server
 Invoke-Command -computername Server03 -scriptblock {Get-HotFix | sort InstalledOn}
-
-#Show last patch/update time and date multiple servers
-Invoke-Command -computername server01,server02,server03 -scriptblock {Get-HotFix | sort InstalledOn}
-
-#optional I perfer the cmds above
-#Obtain a list of installed Updates using wmic
-wmic qfe list
-#or
-Invoke-Command -ComputerName "server03" -ScriptBlock {wmic qfe list}
-
-
-
+#
 #Example
 Source        Description      HotFixID      InstalledBy          InstalledOn                PSComputerName                 
 ------        -----------      --------      -----------          -----------                --------------                                     
@@ -24,3 +13,12 @@ SERVER3       Security Update  KB5023789     NT AUTHORITY\SYSTEM  3/21/2023 12:0
 SERVER3       Update           KB5028855     NT AUTHORITY\SYSTEM  8/2/2023 12:00:00 AM       Server3                        
 SERVER3       Security Update  KB5028316     NT AUTHORITY\SYSTEM  8/2/2023 12:00:00 AM       Server3                        
 SERVER3       Security Update  KB5028168     NT AUTHORITY\SYSTEM  8/2/2023 12:00:00 AM       Server3    
+#
+#Show last patch/update time and date multiple servers
+Invoke-Command -computername server01,server02,server03 -scriptblock {Get-HotFix | sort InstalledOn}
+#
+#optional I perfer the cmds above
+#Obtain a list of installed Updates using wmic
+wmic qfe list
+#or
+Invoke-Command -ComputerName "server03" -ScriptBlock {wmic qfe list}
