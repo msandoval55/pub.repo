16c17871c288a86ac9f4a95790e60df0950dbb49
msandoval55 <116230991+msandoval55@users.noreply.github.com>
2023-08-17 09:37:44 -0500

    commit and push

--- Microsoft Windows Server/Logoff-user.ps1
+++ Microsoft Windows Server/Logoff-user.ps1
@@ -1,2 +1,14 @@
 #log user from server
+<<<<<<< HEAD
+Invoke-Command -ComputerName server01 -ScriptBlock {logoff '#' /server:$SERVER}
+
+=============================================================================================
+#kill tasks of user id to log user off
+Invoke-Command -ComputerName server01 -ScriptBlock {taskkill /FI "SESSION eq ID#" /F}
+
+#Example
+SUCCESS: The process with PID 18880 has been terminated.
+SUCCESS: The process with PID 11368 has been terminated.
+SUCCESS: The process with PID 26932 has been terminated.
+=======
 Invoke-Command -ComputerName server01 -ScriptBlock {logoff '#' /server:$SERVER}
