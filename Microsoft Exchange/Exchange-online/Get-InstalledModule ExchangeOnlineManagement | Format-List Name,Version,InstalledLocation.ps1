b78fdda2d98831ade307e3713bc014d97d2645d7
msandoval55 <116230991+msandoval55@users.noreply.github.com>
2023-08-17 13:49:28 -0500

    commit and push

---
+++ Microsoft Exchange/Exchange-online/Get-InstalledModule ExchangeOnlineManagement | Format-List Name,Version,InstalledLocation.ps1
@@ -0,0 +1,6 @@
+Get-InstalledModule ExchangeOnlineManagement | Format-List Name,Version,InstalledLocation
+
+#example
+Name              : ExchangeOnlineManagement
+Version           : 3.2.0
+InstalledLocation : C:\Program Files\WindowsPowerShell\Modules\ExchangeOnlineManagement\3.2.0
