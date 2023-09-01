2fac4e4be6026f3dcfb49d6f6d83fd86a81e010a
msandoval55 <116230991+msandoval55@users.noreply.github.com>
2023-09-01 09:06:22 -0500

    Commit & Push

---
+++ Microsoft 365/Licensing/Get-MsolAccountSku ServiceStatus.ps1
@@ -0,0 +1,1 @@
+Get-MsolAccountSku | select -ExpandProperty ServiceStatus
--- Microsoft 365/Licensing/Get-MsolAccountSku.ps1
+++ Microsoft 365/Licensing/Get-MsolAccountSku.ps1
@@ -29,3 +29,21 @@ PROJECT_MADEIRA_PREVIEW_IW_SKU  10000       0            0
 PHONESYSTEM_VIRTUALUSER_FACULTY 100         100          11           
 RIGHTSMANAGEMENT_ADHOC          50000       0            0            
 POWERAPPS_DEV                   10000       0            4            
+
+
+https://learn.microsoft.com/en-us/microsoft-365/enterprise/view-licenses-and-services-with-microsoft-365-powershell?view=o365-worldwide
+
+The cmdlet returns AccountSKU objects that contain the following information:
+
+-AccountName. The name of the account this SKU belongs to.
+-AccountObjectId. The unique ID of the account this SKU belongs to.
+-AccountSkuId. The unique string ID of the account/SKU combination. This value should be used when assigning or updating licenses.
+-ActiveUnits. The number of active licenses.
+-ConsumedUnits. The number of licenses consumed.
+-ServiceStatus. The provisioning status of individual services belonging to this SKU.
+-SkuId. The unique ID for the SKU.
+-SkuPartNumber. The partner number of this SKU.
+-SubscriptionIds. A list of all subscriptions associated with this SKU. For the purposes of assigning licenses, all subscriptions with the same SKU will be grouped into a single license pool.
+-SuspendedUnits. The number of suspended licenses. These licenses are not available for assignment.
+-TargetClass. The target class of this SKU. Only SKUs with target class=user are assignable.
+-WarningUnits. The number of warning units.
