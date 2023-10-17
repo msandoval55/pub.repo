6756f3681043c1bc0f3add1d4779c9a35a9b374d
msandoval55 <116230991+msandoval55@users.noreply.github.com>
2023-10-17 13:30:38 -0500

    Update Logoff-user.ps1

--- Microsoft Windows Server/Logoff-user.ps1
+++ Microsoft Windows Server/Logoff-user.ps1
@@ -1,3 +1,24 @@
+#get users logged on to server
+Invoke-Command -ComputerName Server1 -ScriptBlock {query user /server:$SERVER}
+
+#Example
+ USERNAME        SESSIONNAME        ID STATE   IDLE TIME  LOGON TIME
+ logan2          rdp-tcp#39         2  Active       1:10  8/3/2023 1:18 PM
+ martin55                           3  Disc        15:17  8/3/2023 1:21 PM
+ lugo85                             4  Disc      1+01:06  8/3/2023 1:22 PM
+ gonzalez16                         5  Disc         1:22  8/3/2023 1:41 PM
+ salazar38       rdp-tcp#65         7  Active          7  8/4/2023 9:03 AM
+
+=============================================================================================
+#get users and sessions logged on to server
+Invoke-Command -ComputerName hybrid1 -ScriptBlock {qwinsta}
+
+#Example
+SESSIONNAME       USERNAME                 ID  STATE   TYPE        DEVICE
+>services                                    0  Disc
+ console                                     2  Conn
+ rdp-tcp                                 65536  Listen
+
 #log user from server
 Invoke-Command -ComputerName server01 -ScriptBlock {logoff '#' /server:$SERVER}
 
