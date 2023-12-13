204510a12415ee9e790e20af0c26bc7521af7cc1
msandoval55 <116230991+msandoval55@users.noreply.github.com>
2023-12-13 09:27:01 -0600

    Update Get-ADUser by group memberof.ps1

--- Microsoft Active Directory/AD Users and Computers/Users/Get-ADUser by group memberof.ps1
+++ Microsoft Active Directory/AD Users and Computers/Users/Get-ADUser by group memberof.ps1
@@ -1,2 +1,2 @@
 #Get-ADUser groups (memberof)
-Get-ADUser "ad.username" -Properties MemberOf).MemberOf | Get-ADObject -Properties Name | Sort Name | ft Name
+(Get-ADUser "ad.username" -Properties MemberOf).MemberOf | Get-ADObject -Properties Name | Sort Name | ft Name
