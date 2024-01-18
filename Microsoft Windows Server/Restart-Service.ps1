40e21cebc9d8946619e7cb3d975208c3d5a23732
msandoval55 <116230991+msandoval55@users.noreply.github.com>
2024-01-18 08:05:23 -0600

    Update Restart-Service.ps1

--- Microsoft Windows Server/Restart-Service.ps1
+++ Microsoft Windows Server/Restart-Service.ps1
@@ -1,5 +1,5 @@
 #get-service invoke cmdlet
-Invoke-Command -ComputerName lsmssql -ScriptBlock {Get-Service 'sqlserveragent'}
+Invoke-Command -ComputerName lsmssql -ScriptBlock {Get-Service 'sqlserveragent' | ft -autosize}
 
 #output example
 Status   Name               DisplayName                            PSComputerName
