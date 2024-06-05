acc01d6ba693fb1c3aa6d5f8d950e6e28a876bb1
msandoval55 <116230991+msandoval55@users.noreply.github.com>
2024-06-05 09:12:47 -0500

    Update Check AD User Lockout.ps1

--- Microsoft Active Directory/AD Users and Computers/Lockouts/Check AD User Lockout.ps1
+++ Microsoft Active Directory/AD Users and Computers/Lockouts/Check AD User Lockout.ps1
@@ -1,9 +1,13 @@
 #You can check if the account is locked using the ADUC graphical console or with the Get-ADUser cmdlet from the Active Directory module for PowerShell:
 Get-ADUser -Identity jsmith -Properties LockedOut,DisplayName | Select-Object samaccountName, displayName,Lockedout
+
 #Or:
+
 Get-ADUser jsmith -Properties Name,Lockedout, lastLogonTimestamp,lockoutTime,logonCount,pwdLastSet | Select-Object Name, Lockedout,@{n='LastLogon';e={[DateTime]::FromFileTime($_.lastLogonTimestamp)}},@{n='lockoutTime';e={[DateTime]::FromFileTime($_.lockoutTime)}},@{n='pwdLastSet';e={[DateTime]::FromFileTime($_.pwdLastSet)}},logonCount
+
 #You can list all currently locked accounts in a domain using the Search-ADAccount cmdlet:
 Search-ADAccount -UsersOnly -lockedout
+
 #You can check the account lockout time, the number of failed password logon attempts, and the time of the last successful logon in the account properties in the ADUC console (on the Attribute Editor tab) or using PowerShell:
 Get-ADUser jsmith -Properties Name, lastLogonTimestamp,lockoutTime,logonCount,pwdLastSet | Select-Object Name,@{n='LastLogon';e={[DateTime]::FromFileTime($_.lastLogonTimestamp)}},@{n='lockoutTime';e={[DateTime]::FromFileTime($_.lockoutTime)}},@{n='pwdLastSet';e={[DateTime]::FromFileTime($_.pwdLastSet)}},logonCount
 
