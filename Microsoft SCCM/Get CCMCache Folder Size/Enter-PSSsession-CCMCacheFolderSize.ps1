3f8abda81ba1d8cb84aa9f089673c84f3423fe00
msandoval55 <116230991+msandoval55@users.noreply.github.com>
2024-11-20 09:29:22 -0600

    Delete Microsoft SCCM/Get CCMCache Folder Size directory

--- Microsoft SCCM/Get CCMCache Folder Size/Enter-PSSsession-CCMCacheFolderSize.ps1
+++
@@ -1,5 +0,0 @@
-#Get folder size from remote server
-#enter powershell session
-Enter-PSSession servername
-#run script to get folder size in Gb
-Get-ChildItem -Path C:\windows\ccmcache -Recurse | Measure-Object -Sum Length | Select-Object @{name='folder size (Gb)';expression={$_.Sum/1gb}}
--- Microsoft SCCM/Get CCMCache Folder Size/Invoke-CCMCacheFolderSize.ps1
+++
@@ -1,2 +0,0 @@
-#Get folder size from remote server
-Invoke-Command -ComputerName "servername" -ScriptBlock {Get-ChildItem -Path C:\windows\ccmcache -Recurse | Measure-Object -Sum Length | Select-Object @{name='folder size (Gb)';expression={$_.Sum/1gb}}}
