a91aed45d1dd5c74311afeb8359cbcbc2f61760d
msandoval55 <116230991+msandoval55@users.noreply.github.com>
2025-03-26 08:17:32 -0500

    Update Get-SystemLogs.ps1

--- Microsoft Windows Server/Event Viewer/Get-SystemLogs.ps1
+++ Microsoft Windows Server/Event Viewer/Get-SystemLogs.ps1
@@ -1,4 +1,4 @@
-#Get systems logs from a remote server with a time frame
+#Get systems logs from a remote server with a time frame and out-gridview report
 Invoke-Command -ComputerName Servername -ScriptBlock {
     Get-WinEvent -LogName System -ErrorAction SilentlyContinue |
     Where-Object { $_.TimeCreated -ge (Get-Date "3/24/2025 04:00:00 AM") -and $_.TimeCreated -le (Get-Date "3/24/2025 04:15:00 AM") }
