a07257a9ea3554a4c9f713805a63ebaa7bc84be6
msandoval55 <116230991+msandoval55@users.noreply.github.com>
2025-03-26 08:19:33 -0500

    Update Find VMs with ISOs Attached.ps1

--- VMware/Find VMs with ISOs Attached.ps1
+++ VMware/Find VMs with ISOs Attached.ps1
@@ -1,4 +1,4 @@
-# Connect to vCenter
+#Connect to vCenter
 connect-viserver servername
-# Get VMs with ISOs attached and export to CSV
+#Get VMs with ISOs attached and export to CSV
 get-vm | Get-CDDrive | select @{N="VM";E="Parent"},IsoPath | where {$_.IsoPath -ne $null} | export-csv -path "C:\your\path.csv"
