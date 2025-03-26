9468d1553ca7fa654728526ba055b9df0e802414
msandoval55 <116230991+msandoval55@users.noreply.github.com>
2025-03-26 13:37:51 -0500

    Update Search-ADAccount all lockedout users.ps1

--- Microsoft Active Directory/AD Users and Computers/Lockouts/Search-ADAccount all lockedout users.ps1
+++ Microsoft Active Directory/AD Users and Computers/Lockouts/Search-ADAccount all lockedout users.ps1
@@ -1,2 +1,9 @@
 #Search for all locked-out users with a table
 Search-ADAccount -UsersOnly -lockedout | FT -AutoSize Enabled,Lockedout,Name,SamAccountName,UserPrincipalName,LastLogonDate
+
+#With out-gridview
+Search-ADAccount -LockedOut -UsersOnly | 
+    Select-Object Enabled, LockedOut, Name, SamAccountName, UserPrincipalName, LastLogonDate | 
+    Sort-Object LastLogonDate -Descending | 
+    Out-GridView -Title "Locked Out User Accounts"
+
