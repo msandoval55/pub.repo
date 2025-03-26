ffa38517b342fe9496e92badc4616be328e06b54
msandoval55 <116230991+msandoval55@users.noreply.github.com>
2025-03-26 13:46:03 -0500

    Update Get-ADUser all properties.ps1

--- Microsoft Active Directory/AD Users and Computers/Users/Get-ADUser all properties.ps1
+++ Microsoft Active Directory/AD Users and Computers/Users/Get-ADUser all properties.ps1
@@ -1,2 +1,11 @@
 #Get-ADUser All Properties
 Get-ADUser -Identity username -Properties *
+
+
+#With multiple users and out-gridview
+$Users = @("user1", "user2", "user3")
+# Dynamically construct the filter string
+$UserFilter = ($Users | ForEach-Object { "(SamAccountName -eq '$_')" }) -join " -or "
+# Run the Get-ADUser command with the dynamic filter
+Get-ADUser -Filter $UserFilter -Properties LastLogonDate, EmailAddress, Enabled | 
+    Out-GridView -Title "Multiple User Details"
