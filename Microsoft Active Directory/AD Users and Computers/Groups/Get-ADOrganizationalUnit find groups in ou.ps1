689709179e192091c68cbb59052c6328eccbc2b0
msandoval55 <116230991+msandoval55@users.noreply.github.com>
2025-11-05 10:08:33 -0600

    Update Get-ADOrganizationalUnit find groups in ou.ps1

--- Microsoft Active Directory/AD Users and Computers/Groups/Get-ADOrganizationalUnit find groups in ou.ps1
+++ Microsoft Active Directory/AD Users and Computers/Groups/Get-ADOrganizationalUnit find groups in ou.ps1
@@ -3,3 +3,8 @@ Get-ADOrganizationalUnit -Filter * -SearchBase "OU=OU.Folder.Name, OU=Domain.nam
 
 #Find Groups in a OU with Name Only
 Get-ADOrganizationalUnit -Filter * -SearchBase "OU=OU.Folder.Name, OU=Domain.name, DC=Domain.name, DC=edu.or.com" | Get-ADObject -Properties Name | Format Table Name
+
+
+$OU = "OU=Servers,OU="",DC="",DC="",DC="""
+
+Get-ADComputer -SearchBase $OU -SearchScope Subtree -Filter * | Select-Object Name, DNSHostName
